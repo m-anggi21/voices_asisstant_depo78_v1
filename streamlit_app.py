f7628cae079654cbef756b70a036a0bb7d724e1b
Muhamad Anggi Saputra <muhamadanggi2104@gmail.com>
2026-01-08 10:44:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Depo 78",
+    page_icon="🛒",
+    layout="centered",
+    initial_sidebar_state="collapsed"
 )
+
+# INIT AUTH
+if "auth" not in st.session_state:
+    st.session_state.auth = {"is_logged_in": False, "user": None}
+
+# LANGSUNG PINDAH KE LOGIN
+st.switch_page("pages/1_Login.py")
+st.stop()
